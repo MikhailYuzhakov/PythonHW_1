303d9383aca56c5464362d6fdd6b1cf2348e2769
MikhailYuzhakov <miha.yuzhakov@gmail.com>
2022-07-16 09:46:03 +0300

    Точки в заданной четверти и дипазоны

--- Task4.py
+++ Task4.py
@@ -7,23 +7,26 @@ quad = int(input())
 if (quad < 1 or quad > 4):
     print("Вы ввели некорректные входные данные")
 else:
-    print("Возможные точки из заданной четверти: ")
     if (quad == 1):
+        print("X ∈ [0, +∞); Y ∈ [0, +∞). Например, точки: ")
         for i in range(10):
             x = randint(1, 10) * i
             y = randint(1, 10) * i
             print("(",x,";",y,")")
     if (quad == 2):
+        print("X ∈ [-∞, 0); Y ∈ [0, +∞). Например, точки: ")
         for i in range(10):
             x = -1 * randint(1, 10) * i
             y = randint(1, 10) * i
             print("(",x,";",y,")")
     if (quad == 3):
+        print("X ∈ [-∞, 0); Y ∈ [-∞, 0). Например, точки: ")
         for i in range(10):
             x = -1 * randint(1, 10) * i
             y = -1 * randint(1, 10) * i
             print("(",x,";",y,")")
     if (quad == 4):
+        print("X ∈ [0, +∞); Y ∈ [-∞, 0). Например, точки: ")
         for i in range(10):
             x = randint(1, 10) * i
             y = -1 * randint(1, 10) * i
